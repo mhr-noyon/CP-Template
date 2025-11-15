ec12b02a5a40a33b95732dccb4d7d0a73012f96d
Md. Hosain Rohman Noyon <91529035+mhr-noyon@users.noreply.github.com>
2025-11-15 13:59:48 +0600

    Add stress testing loop with error reporting
    Implemented a loop to run stress tests up to 10,000 times, comparing outputs from different implementations and providing detailed error reporting.

--- stress_testing/stressTesting.sh
+++ stress_testing/stressTesting.sh
@@ -32,3 +32,43 @@ done
 
 #here numTests can be declared using command : 100
 
+
+
+for ((testNum=0;testNum<10000; testNum++)) 
+do 
+    ./$3 > input 
+    ./$2 < input > outSlow 
+    ./$1 < input > outWrong 
+
+    if ! cmp -s "outWrong" "outSlow" 
+    then 
+        echo "❌ Error found on test #$testNum"
+        echo "Input:"
+        cat input 
+        echo ""
+
+         # Show differing line number(s)
+        echo "Line mismatch details:"
+        diff -y --suppress-common-lines outWrong outSlow | nl | head -n 5
+        echo ""
+
+        # Get the first differing line number only
+        lineDiff=$(diff -u outSlow outWrong | grep -m 1 '^@@' | grep -oP '(?<=\+)[0-9]+')
+        if [ -n "$lineDiff" ]; then
+            echo "First mismatch at line: $lineDiff"
+        fi
+        echo ""
+
+
+        echo "Wrong Output:"
+        cat outWrong 
+        echo ""
+        echo "Correct Output:"
+        cat outSlow 
+        echo ""
+        exit 1
+    else
+        echo "Tc: #$testNum"
+    fi 
+done 
+
