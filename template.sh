4417087573564fd19eba95d28a752526a4d6cd86
Md. Hosain Rohman Noyon <91529035+mhr-noyon@users.noreply.github.com>
2026-06-22 14:05:13 +0600

    Update template.sh

--- template.sh
+++ template.sh
@@ -12,7 +12,7 @@ file="$1.cpp"
 # create the file and write the template into it
 cat <<EOF > "$file"
 /*
-    author: mhr-noyon
+    author: lostBit
     Time: $(date +"%H:%M:%S")
     Date: $(date +"%d-%m-%Y")
 */
@@ -20,11 +20,27 @@ cat <<EOF > "$file"
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define ld long double
+#define bigInt __int128
 #define endl "\\n"
 #define pll pair<ll, ll>
+#define pii pair<int, int>
 #define Graph vector<vector<ll>>
 #define inf 1e18
 #define mod 1000000007
+#define PIE acos(-1)
+#define vi vector<int>
+#define vl vector<ll>
+#define vpll vector<pll>
+#define vpii vector<pii>
+#define fori(n) for(int i=0; i<n; i++)
+#define fori1(n) for(int i=1; i<=n; i++)
+#define forj(n) for(int j=0; j<n; j++)
+#define forj1(n) for(int j=1; j<=n; j++)
+#define rfori(n) for(int i=n-1; i>=0; i--)
+#define rfori1(n) for(int i=n; i>=1; i--)
+#define rforj(n) for(int j=n-1; j>=0; j--) 
+#define rforj1(n) for(int j=n; j>=1; j--)
 
 void solve()
 {
